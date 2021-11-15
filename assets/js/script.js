9e6c0816790418dc5ef5dfb618f87455e2e34664
Thais Bertoldi <thais_brt@hotmail.com>
2021-11-15 15:50:05 -0300

    Erro para campos em branco e correções

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,30 @@ class ValidaFormulario {
 
     handleSubmit(e) {
         e.preventDefault();
-        console.log('Formulário não enviado...');
+        const camposValidos = this.camposSaoValidos();
+    }
+
+    camposSaoValidos() {
+        let valid = true;
+
+        for(let errorText of this.formulario.querySelectorAll('.error-text')){
+            errorText.remove();
+        }
+
+        for(let campo of this.formulario.querySelectorAll('.validar')){
+            const label = campo.previousElementSibling.innerText;
+            if(!campo.value) {
+                this.criaErro(campo, `Campo "${label}" não pode estar em branco.`);
+                valid = false;
+            }
+        }
+    }
+
+    criaErro(campo, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('error-text');
+        campo.insertAdjacentElement('afterend', div);
     }
 }
 
